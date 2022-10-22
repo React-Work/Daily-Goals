cec10a2ce4178efb6ff4b0dc6ad7e17d5b2465d6
Parminder Singh <parminder94171@gmail.com>
2022-10-22 19:34:38 +0530

    FEAT : Styed-components

--- src/components/DailyGoal/GoalInput/GoalInput.js
+++ src/components/DailyGoal/GoalInput/GoalInput.js
@@ -1,30 +1,70 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
+import styled from "styled-components";
 
-import Button from '../../UI/Button/Button';
-import './GoalInput.css';
+import Button from "../../UI/Button/Button";
+import "./GoalInput.css";
 
-const GoalInput = props => {
-  const [enteredValue, setEnteredValue] = useState('');
+const FormControl = styled.div`
+    margin: 0.5rem 0;
+    & label {
+        font-weight: bold;
+        display: block;
+        color: ${props => props.inValid ? "red" : "black"};
+        margin-bottom: 0.5rem;
+    }
 
-  const goalInputChangeHandler = event => {
-    setEnteredValue(event.target.value);
-  };
+    & input {
+        display: block;
+        width: 100%;
+        border: 1px solid ${props => props.inValid ? "red" : "#ccc"};
+        background: ${props => props.inValid ? "#ffd7d7" : ""};
+        font: inherit;
+        line-height: 1.5rem;
+        padding: 0 0.25rem;
+    }
 
-  const formSubmitHandler = event => {
-    event.preventDefault();
-    props.onAddGoal(enteredValue);
-    setEnteredValue("")
-  };
+    & input:focus {
+        outline: none;
+        background: #fad0ec;
+        border-color: #8b005d;
+    }
+`;
 
-  return (
-    <form onSubmit={formSubmitHandler}>
-      <div className="form-control">
-        <label>Daily Goal</label>
-        <input type="text" value={enteredValue} onChange={goalInputChangeHandler} />
-      </div>
-      <Button type="submit">Add Goal</Button>
-    </form>
-  );
+const GoalInput = (props) => {
+    const [enteredValue, setEnteredValue] = useState("");
+    const [isValid, setIsValid] = useState(true);
+
+    const goalInputChangeHandler = (event) => {
+        if (!isValid && enteredValue.trim().length > 0) {
+            setIsValid(true);
+            return;
+        }
+        setEnteredValue(event.target.value);
+    };
+
+    const formSubmitHandler = (event) => {
+        event.preventDefault();
+        if (enteredValue.trim().length === 0) {
+            setIsValid(false);
+            return;
+        }
+        props.onAddGoal(enteredValue);
+        setEnteredValue("");
+    };
+
+    return (
+        <form onSubmit={formSubmitHandler}>
+            <FormControl inValid={!isValid}>
+                <label>Daily Goal</label>
+                <input
+                    type="text"
+                    value={enteredValue}
+                    onChange={goalInputChangeHandler}
+                />
+            </FormControl>
+            <Button type="submit">Add Goal</Button>
+        </form>
+    );
 };
 
 export default GoalInput;
